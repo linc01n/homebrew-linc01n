559106d42d13076016538720417561679cfe9a13
Lincoln Lee <git@lincoln.hk>
2016-03-02 02:22:50 +0800

    Update fantastical to 1.3.19

--- Casks/fantastical1.rb
+++ Casks/fantastical1.rb
@@ -1,10 +1,10 @@
 cask 'fantastical1' do
-  version '1.3.18'
-  sha256 '00069dca92be66b04351cb9b19146c305696fb5970b33e71c1b2d687bbc5f2a8'
+  version '1.3.19'
+  sha256 'a0c8f05072c686aeedd79640ceb887a2b07d6c1f6378b88e53ecdbcd2cb26eec'
 
   url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
   appcast 'https://flexibits.com/fantastical/appcast.php',
-          :sha256 => '3d789ff8dd91c8ebc1b63d2811ec17e0a950433b03605d8c10eef010733cf79a'
+          :sha256 => '24bfd6281e906d1e15826fcee41e7a65517e8add89776862cdb41b60c281d3ce'
   name 'Fantastical'
   homepage 'http://flexibits.com/fantastical'
   license :commercial
